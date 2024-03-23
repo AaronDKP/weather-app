3b3ba9e190e0ad75b826f760f9338a7cb4381ba4
AaronDKP <138345602+AaronDKP@users.noreply.github.com>
2024-03-23 17:27:43 +1000

    dist subtree commit

--- webpack.config.js
+++ webpack.config.js
@@ -16,15 +16,6 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'img/',
-            },
-          },
-        ],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
